d38c41813208970c985eb4252618ad4cd0f552cd
Nael Shaban <nael.shaban@gmail.com>
2020-06-30 07:22:52 +0300

    change wording to test pipeline

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ port = int(os.environ.get("PORT", 5000))
 @app.route('/')
 def hello_world():
     visitors_address = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
-    
+
     now = datetime.now()
     dt_string = now.strftime("%d.%m.%Y %H:%M:%S")
-    
-    return 'Current date and time is: '+ dt_string +'\nYour IP address is: ' + visitors_address
+
+    return 'Date & time is: '+ dt_string +'\nYour IP address is: ' + visitors_address
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=port)
